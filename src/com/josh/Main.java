5dd5f7a7964ab8290ab49392f68251e3ec8d21c8
kortasthekiller <kortas@gmail.com>
2016-03-12 21:44:01 -0600

    Initial commit

---
+++ src/com/josh/Main.java
@@ -0,0 +1,22 @@
+package com.josh;
+
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ArrayList<String> deck = new ArrayList<>();
+    deck.add("clubs A");
+        deck.add("clubs 2");
+        deck.add("clubs 3");
+
+
+    }
+}
+//DO this to get the deck of cards setup and then deal out the cards and set them up with arrays for the player hand
+//and the computer hand and then remove them from the 'deck' as I do this?
+
+//As we just learned classes would I setup a 'hand' class and then setup a player.hand as well as a computer.hand and
+//from there?   Adding to these and removing from the deck, etc.
+
+//Thanks!//
